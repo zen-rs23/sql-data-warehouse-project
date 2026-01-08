712c8ec3f31f8cdcdb6b1a435f53abb648968c08
zen-rs23 <k.rohitsharma2311@gmail.com>
2026-01-08 16:20:19 +0530

    Update and rename quality_checks to quality_checks.sql

--- tests/quality_checks.sql
+++ tests/quality_checks.sql
@@ -1,6 +1,6 @@
 /*
 -------------------------------------------------------------------------------
-Quality Checks
+Quality Checks - Bronze & Silver Layer
 ===============================================================================
 Script Purpose:
   This script performs various quality checks for data consistency, accuracy, and standardization across the silver schems. It include checks for:
@@ -188,3 +188,56 @@ WHERE cat != TRIM(cat) OR subcat != TRIM(subcat) OR maintenance != TRIM(maintena
 SELECT DISTINCT
 cat, subcat, maintenance
 FROM bronze.erp_px_cat_g1v2 
+
+/*
+===============================================================================
+Quality Checks - Gold Layer
+===============================================================================
+Script Purpose:
+    This script performs quality checks to validate the integrity, consistency, 
+    and accuracy of the Gold Layer. These checks ensure:
+    - Uniqueness of surrogate keys in dimension tables.
+    - Referential integrity between fact and dimension tables.
+    - Validation of relationships in the data model for analytical purposes.
+
+Usage Notes:
+    - Investigate and resolve any discrepancies found during the checks.
+===============================================================================
+*/
+
+-- ====================================================================
+-- Checking 'gold.dim_customers'
+-- ====================================================================
+-- Check for Uniqueness of Customer Key in gold.dim_customers
+-- Expectation: No results 
+SELECT 
+    customer_key,
+    COUNT(*) AS duplicate_count
+FROM gold.dim_customers
+GROUP BY customer_key
+HAVING COUNT(*) > 1;
+
+-- ====================================================================
+-- Checking 'gold.product_key'
+-- ====================================================================
+-- Check for Uniqueness of Product Key in gold.dim_products
+-- Expectation: No results 
+SELECT 
+    product_key,
+    COUNT(*) AS duplicate_count
+FROM gold.dim_products
+GROUP BY product_key
+HAVING COUNT(*) > 1;
+
+-- ====================================================================
+-- Checking 'gold.fact_sales'
+-- ====================================================================
+-- Check the data model connectivity between fact and dimensions
+SELECT * 
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+ON c.customer_key = f.customer_key
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+WHERE p.product_key IS NULL OR c.customer_key IS NULL  
+
